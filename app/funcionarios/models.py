aeeda94b29427089117b42bec5c7b48866e15c90
Fabio Junior <fabiojunior96@outlook.com>
2021-05-14 00:58:03 -0300

    Trabalhando com os relacionamentos e chaves extrangeiras

--- app/funcionarios/models.py
+++ app/funcionarios/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from app.departamentos.models import Departamento
+from app.empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text="Nome do funcionário")
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
